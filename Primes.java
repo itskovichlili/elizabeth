6ede7f59847b83b2c58aa0ca24682afd9e57add1
Elizabeth <elizabeti@ariel.ac.il>
2021-10-23 00:23:29 +0300

    Initial commit

--- Primes.java
+++ Primes.java
@@ -24,6 +24,7 @@ package prime;
 public class Primes {
     public static boolean isPrime(int n) {
         boolean ans = true;
+		if (n==2 || n== 3) ans = true;
         for (int i = 2; ans && i <= Math.sqrt(n); i++) {
             if (n % i == 0) ans = false;
         }
