36fe8d73943740c86adab7d8dd970427d9d8b117
itskovichlili <57544385+itskovichlili@users.noreply.github.com>
2021-10-30 01:49:26 +0300

    Add files via upload

---
+++ Primes.java
@@ -0,0 +1,19 @@
+package prime;
+
+public class Primes {
+    public static boolean isPrime(int n) {
+		// check if n is a prime number;
+		// check only odd numbers for good performance
+		// go to sqrt(n)
+        boolean ans = true;
+        if (n==2) return true;
+        if (n%2==0)  return false;
+        if (n<=7) return true;
+        for (int i=3; ans && i<=Math.sqrt(n); i=i+2) {
+            if (n % i == 0) ans = false;
+        }
+        return ans;
+    }
+}
+"" 
+"more text" 
